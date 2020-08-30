009d5c32917b0be3d22313591a44a207e3f7e5c2
fapple <sd1234800@gmail.com>
2020-08-30 23:23:13 +0800

    add bootstrap  and a week weather

--- index.php
+++ index.php
@@ -12,68 +12,124 @@ if(isset($_POST["location"])){
     $_SESSION["location"]="臺北市";
 }
 
-// require_once "test.php";
-// if(isset($_POST["okbtn2"])){
-//     // echo "ok";
-//     $location=$_POST["location"];
-
-//     if(trim($location)!= ""){
-        
-//         $_SESSION["location"]=$location;
-//         $_SESSION["datetime"]=date("Y-m-d H:i:s");
-//         header("location: test2.php");
-//     }
-// }
+if(isset($_POST["okbtn"])){
+    $location = $_POST["location"];
+    $_SESSION["location"]=$location;
+}
 ?>
 
 <!DOCTYPE html>
 <html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <form method="post">
-        <select name= "location" class="form-control" onchange="this.form.submit()">
-            <optgroup label="北部">
-            <option <?php if($location== '臺北市'){echo "selected";}?> >臺北市</option>
-            <option <?php if($location== '新北市'){echo "selected";}?> >新北市</option>
-            <option <?php if($location== '基隆市'){echo "selected";}?> >基隆市</option>
-            <option <?php if($location== '宜蘭縣'){echo "selected";}?> >宜蘭縣</option>
-            <option <?php if($location== '桃園市'){echo "selected";}?> >桃園市</option>
-            <option <?php if($location== '新竹縣'){echo "selected";}?> >新竹縣</option>
-            <option <?php if($location== '新竹市'){echo "selected";}?> >新竹市</option>
-            </optgroup>
-            <optgroup label="中部">
-            <option <?php if($location== '臺中市'){echo "selected";}?> >臺中市</option>
-            <option <?php if($location== '南投縣'){echo "selected";}?> >南投縣</option>
-            <option <?php if($location== '苗栗縣'){echo "selected";}?> >苗栗縣</option>
-            <option <?php if($location== '彰化縣'){echo "selected";}?> >彰化縣</option>
-            <option <?php if($location== '雲林縣'){echo "selected";}?> >雲林縣</option>
-            </optgroup>
-            <optgroup label="南部">
-            <option <?php if($location== '高雄市'){echo "selected";}?> >高雄市</option>
-            <option <?php if($location== '臺南市'){echo "selected";}?> >臺南市</option>
-            <option <?php if($location== '屏東縣'){echo "selected";}?> >屏東縣</option>
-            <option <?php if($location== '嘉義縣'){echo "selected";}?> >嘉義縣</option>
-            <option <?php if($location== '嘉義市'){echo "selected";}?> >嘉義市</option>
-            </optgroup>
-            <optgroup label="東部">
-            <option <?php if($location== '花蓮縣'){echo "selected";}?> >花蓮縣</option>
-            <option <?php if($location== '臺東縣'){echo "selected";}?> >臺東縣</option>
-            </optgroup>
-            <optgroup label="外島">
-            <option <?php if($location== '金門縣'){echo "selected";}?> >金門縣</option>
-            <option <?php if($location== '連江縣'){echo "selected";}?> >連江縣</option>
-            <option <?php if($location== '澎湖縣'){echo "selected";}?> >澎湖縣</option>
-            </optgroup>
-        </select>
-        <input type="submit" name="okbtn" value="未來一周"></input>
-        
-    </form>
-    <?php require "test.php"?>
-    
-    
-</body>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <title>Bootstrap 4, from LayoutIt!</title>
+
+    <meta name="description" content="Source code generated using layoutit.com">
+    <meta name="author" content="LayoutIt!">
+
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+
+  </head>
+  <body>
+
+    <div class="container-fluid">
+	<div class="row">
+		<div class="col-md-12">
+			<h3>
+				氣象站
+			</h3>
+			<div class="jumbotron">
+				<h2>
+					Hello, world!
+				</h2>
+				<form method="post">
+					<select name= "location" class="form-control" onchange="this.form.submit()">
+						<optgroup label="北部">
+						<option <?php if($location== '臺北市'){echo "selected";}?> >臺北市</option>
+						<option <?php if($location== '新北市'){echo "selected";}?> >新北市</option>
+						<option <?php if($location== '基隆市'){echo "selected";}?> >基隆市</option>
+						<option <?php if($location== '宜蘭縣'){echo "selected";}?> >宜蘭縣</option>
+						<option <?php if($location== '桃園市'){echo "selected";}?> >桃園市</option>
+						<option <?php if($location== '新竹縣'){echo "selected";}?> >新竹縣</option>
+						<option <?php if($location== '新竹市'){echo "selected";}?> >新竹市</option>
+						</optgroup>
+						<optgroup label="中部">
+						<option <?php if($location== '臺中市'){echo "selected";}?> >臺中市</option>
+						<option <?php if($location== '南投縣'){echo "selected";}?> >南投縣</option>
+						<option <?php if($location== '苗栗縣'){echo "selected";}?> >苗栗縣</option>
+						<option <?php if($location== '彰化縣'){echo "selected";}?> >彰化縣</option>
+						<option <?php if($location== '雲林縣'){echo "selected";}?> >雲林縣</option>
+						</optgroup>
+						<optgroup label="南部">
+						<option <?php if($location== '高雄市'){echo "selected";}?> >高雄市</option>
+						<option <?php if($location== '臺南市'){echo "selected";}?> >臺南市</option>
+						<option <?php if($location== '屏東縣'){echo "selected";}?> >屏東縣</option>
+						<option <?php if($location== '嘉義縣'){echo "selected";}?> >嘉義縣</option>
+						<option <?php if($location== '嘉義市'){echo "selected";}?> >嘉義市</option>
+						</optgroup>
+						<optgroup label="東部">
+						<option <?php if($location== '花蓮縣'){echo "selected";}?> >花蓮縣</option>
+						<option <?php if($location== '臺東縣'){echo "selected";}?> >臺東縣</option>
+						</optgroup>
+						<optgroup label="外島">
+						<option <?php if($location== '金門縣'){echo "selected";}?> >金門縣</option>
+						<option <?php if($location== '連江縣'){echo "selected";}?> >連江縣</option>
+						<option <?php if($location== '澎湖縣'){echo "selected";}?> >澎湖縣</option>
+						</optgroup>
+					</select>
+					<input type="submit" name="okbtn" value="未來一周"></input>
+					
+				</form>
+			</div>
+			<div class="tabbable" id="tabs-89841">
+				<ul class="nav nav-tabs">
+					<li class="nav-item">
+						<a class="nav-link" href="#tab1" data-toggle="tab">最近兩日</a>
+						<?php require "test.php"?>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link active show" href="#tab2" data-toggle="tab">未來一周</a>
+					</li>
+				</ul>
+				<div class="tab-content">
+					<div class="tab-pane active" id="panel-175523">
+					</div>
+					<div class="tab-pane" id="tab2">
+						<p>
+							<?php require "test2.php"?>
+						</p>
+					</div>
+				</div>
+			</div>
+			<div class="row">
+				<div class="col-md-4">
+					<div class="card">
+						<img class="card-img-top" alt="Bootstrap Thumbnail First" src="https://www.layoutit.com/img/people-q-c-600-200-1.jpg">
+						<div class="card-block">
+							<h5 class="card-title">
+								test1
+							</h5>
+							<p class="card-text">
+								test2
+							</p>
+							<p>
+								<a class="btn btn-primary" href="#">Action</a> 
+							</p>
+						</div>
+					</div>
+				</div>
+				
+			</div>
+		</div>
+	</div>
+</div>
+
+    <script src="js/jquery.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/scripts.js"></script>
+  </body>
 </html>
---
+++ test2.php
@@ -0,0 +1,51 @@
+<?php
+// header("content-type: text/html; charset=utf-8");
+
+$location=$_SESSION["location"];
+$date=$_SESSION["datetime"];
+$weekd=date("Y-m-d H:i:s" ,strtotime("1 week"));
+
+
+// echo "$tormd"."<br>$acqd";
+// 1. 初始設定
+$ch = curl_init();
+$url = "https://opendata.cwb.gov.tw/api/v1/rest/datastore/F-D0047-091?Authorization=CWB-B49B9DCF-7BB9-4300-8452-B146577EE1AE&elementName=WeatherDescription&locationName=$location";
+// 2. 設定 / 調整參數
+
+curl_setopt($ch, CURLOPT_URL,$url);
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_HEADER, 0);
+
+// 3. 執行，取回 response 結果
+$result = curl_exec($ch);
+// 4. 關閉與釋放資源
+curl_close($ch);
+$result=file_get_contents($url);
+$array = json_decode($result,true);
+
+// var_dump($array);
+// echo"$date";
+    echo"未來一周:<br>";
+    for($i=0;$i<14;$i++){
+        $start=$array["records"]["locations"][0]["location"][0]["weatherElement"][0]["time"][$i]["startTime"];
+        $end=$array["records"]["locations"][0]["location"][0]["weatherElement"][0]["time"][$i]["endTime"];
+        // echo "date:"."$date"."<br>start:"."$start"."<br>"."$end";
+        
+        for($x=0;$x<7;$x++){
+            $setday=date("Y-m-d H:i:s" ,strtotime("$x day"));
+            $y=$x+1;
+            if($setday>$start && $setday<$end){
+                $narr = explode("。",$array["records"]["locations"][0]["location"][0]["weatherElement"][0]["time"][$i]["elementValue"][0]["value"]);
+                echo "第"."$y"."天<br>";
+                foreach($narr as $key => $val){
+                        echo("$narr[$key]<br>");
+                }
+            }
+            
+        }
+        
+    }         
+
+// echo $array["records"]["locations"][0]["location"][0]["weatherElement"][0]["time"][0]["elementValue"][0]["value"];
+
+?>
