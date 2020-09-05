93ea42d61bcb0f93390fafbf16b9b2c8c365fef3
fapple <sd1234800@gmail.com>
2020-09-05 19:04:13 +0800

    beautify menu and fix some problem

--- index.php
+++ index.php
@@ -27,7 +27,7 @@ if($two>@$row_search["startT"] && $two<@$row_search["endT"]){
 }else{
 	echo "update or inserting &nbsp";
 	require "insert.php";
-	header("location index.php");
+	header("location: index.php");
 }
 $search_week="select startT,endT from week
 where cityName='$location' and '$weekday' between startT and endT";
@@ -39,7 +39,7 @@ if($weekday>@$row_forweek["startT"] && $weekday<@$row_forweek["endT"]){
 }else{
 	echo "update or inserting week data";
 	require "weekinsert.php";
-	header("location index.php");
+	header("location: index.php");
 }
 // require "weekinsert.php";
 $search_rain="select today from rain";
@@ -80,6 +80,18 @@ if($row_rain["today"]!=$date){
     </script>
 	
 	<style type="text/css">
+	.h3{
+		font-family:PMingLiU;
+	}
+	.h2{
+		color:white;
+		font-family:DFKai-sb;
+	}
+	.select{
+		font-family:DFKai-sb;
+		width:320px;
+		font-size:19px;
+	}
 	.jumbotron{
 		padding:10rem 2rem;
 	}
@@ -92,11 +104,29 @@ if($row_rain["today"]!=$date){
 		height : auto;
 		height : 250px;
 	}
+	.resize_week{
+		height : 50px;
+		height : 50px;
+	}
+	.table_dark{
+		background-color:black;
+		color:white;
+	}
+	.boxblur{
+		width:350px;
+		height:80px;
+		background:rgba(0,0,139,0.6);
+		box-shadow:0 0 20px 10px rgba(0,0,139,1),inset 0 0 20px 10px rgba(0,0,139,1);
+		-webkit-transform:scale(.85);
+		transform:scale(.85);
+	}
+	.nav_a{
+		font-family:Arial,Impact;
+	}
+	.tab{
+		font-family:"Comic Sans MS";
+	}
 	</style>
-
-	
-
-	
 
   </head>
   <body>
@@ -104,67 +134,71 @@ if($row_rain["today"]!=$date){
     <div class="container-fluid">
 	<div class="row">
 		<div class="col-md-12" >
-			<h3>
+			<h3 class="h3">
 				氣象站
 			</h3>
 			<div class="jumbotron" style="background-image:url('./img/<?=$location?>.jpg');">
-				<h2 style="color:white;">
-					臺灣各縣市
-				</h2>
-				<form method="post">
-					<select name= "location" class="form-control" style="width:350px;" onchange="this.form.submit()">
-						<optgroup label="北部">
-						<option <?php if($location== '臺北市'){echo "selected";}?> >臺北市</option>
-						<option <?php if($location== '新北市'){echo "selected";}?> >新北市</option>
-						<option <?php if($location== '基隆市'){echo "selected";}?> >基隆市</option>
-						<option <?php if($location== '宜蘭縣'){echo "selected";}?> >宜蘭縣</option>
-						<option <?php if($location== '桃園市'){echo "selected";}?> >桃園市</option>
-						<option <?php if($location== '新竹縣'){echo "selected";}?> >新竹縣</option>
-						<option <?php if($location== '新竹市'){echo "selected";}?> >新竹市</option>
-						</optgroup>
-						<optgroup label="中部">
-						<option <?php if($location== '苗栗縣'){echo "selected";}?> >苗栗縣</option>
-						<option <?php if($location== '臺中市'){echo "selected";}?> >臺中市</option>
-						<option <?php if($location== '彰化縣'){echo "selected";}?> >彰化縣</option>
-						<option <?php if($location== '南投縣'){echo "selected";}?> >南投縣</option>
-						<option <?php if($location== '雲林縣'){echo "selected";}?> >雲林縣</option>
-						</optgroup>
-						<optgroup label="南部">
-						<option <?php if($location== '嘉義縣'){echo "selected";}?> >嘉義縣</option>
-						<option <?php if($location== '嘉義市'){echo "selected";}?> >嘉義市</option>
-						<option <?php if($location== '臺南市'){echo "selected";}?> >臺南市</option>
-						<option <?php if($location== '高雄市'){echo "selected";}?> >高雄市</option>
-						<option <?php if($location== '屏東縣'){echo "selected";}?> >屏東縣</option>
-						</optgroup>
-						<optgroup label="東部">
-						<option <?php if($location== '花蓮縣'){echo "selected";}?> >花蓮縣</option>
-						<option <?php if($location== '臺東縣'){echo "selected";}?> >臺東縣</option>
-						</optgroup>
-						<optgroup label="外島">
-						<option <?php if($location== '金門縣'){echo "selected";}?> >金門縣</option>
-						<option <?php if($location== '連江縣'){echo "selected";}?> >連江縣</option>
-						<option <?php if($location== '澎湖縣'){echo "selected";}?> >澎湖縣</option>
-						</optgroup>
-					</select>
-					
-				</form>
+				<div class="col-md-5 boxblur" style="border-radius:10px;">
+					<h2 class="h2">
+						臺灣各縣市
+					</h2>
+						<form method="post">
+						<select name= "location" class="form-control select"  onchange="this.form.submit()">
+							<optgroup label="北部">
+							<option <?php if($location== '臺北市'){echo "selected";}?> >臺北市</option>
+							<option <?php if($location== '新北市'){echo "selected";}?> >新北市</option>
+							<option <?php if($location== '基隆市'){echo "selected";}?> >基隆市</option>
+							<option <?php if($location== '宜蘭縣'){echo "selected";}?> >宜蘭縣</option>
+							<option <?php if($location== '桃園市'){echo "selected";}?> >桃園市</option>
+							<option <?php if($location== '新竹縣'){echo "selected";}?> >新竹縣</option>
+							<option <?php if($location== '新竹市'){echo "selected";}?> >新竹市</option>
+							</optgroup>
+							<optgroup label="中部">
+							<option <?php if($location== '苗栗縣'){echo "selected";}?> >苗栗縣</option>
+							<option <?php if($location== '臺中市'){echo "selected";}?> >臺中市</option>
+							<option <?php if($location== '彰化縣'){echo "selected";}?> >彰化縣</option>
+							<option <?php if($location== '南投縣'){echo "selected";}?> >南投縣</option>
+							<option <?php if($location== '雲林縣'){echo "selected";}?> >雲林縣</option>
+							</optgroup>
+							<optgroup label="南部">
+							<option <?php if($location== '嘉義縣'){echo "selected";}?> >嘉義縣</option>
+							<option <?php if($location== '嘉義市'){echo "selected";}?> >嘉義市</option>
+							<option <?php if($location== '臺南市'){echo "selected";}?> >臺南市</option>
+							<option <?php if($location== '高雄市'){echo "selected";}?> >高雄市</option>
+							<option <?php if($location== '屏東縣'){echo "selected";}?> >屏東縣</option>
+							</optgroup>
+							<optgroup label="東部">
+							<option <?php if($location== '花蓮縣'){echo "selected";}?> >花蓮縣</option>
+							<option <?php if($location== '臺東縣'){echo "selected";}?> >臺東縣</option>
+							</optgroup>
+							<optgroup label="外島">
+							<option <?php if($location== '金門縣'){echo "selected";}?> >金門縣</option>
+							<option <?php if($location== '連江縣'){echo "selected";}?> >連江縣</option>
+							<option <?php if($location== '澎湖縣'){echo "selected";}?> >澎湖縣</option>
+							</optgroup>
+						</select>
+						
+					</form>
+				</div>
+				
+				
 			</div>
 			
 			<div class="tabbable" >
 				<ul class="nav nav-tabs">
 					<li class="nav-item">
-						<a class="nav-link active show" href="#tab1" data-toggle="tab">最近兩日</a>
+						<a class="nav-link active show nav_a" href="#tab1" data-toggle="tab">最近兩日</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link " href="#tab2" data-toggle="tab">未來一周</a>
+						<a class="nav-link nav_a" href="#tab2" data-toggle="tab">未來一周</a>
 					</li>
 					<li class="nav-item">
-						<a class="nav-link " href="#tab3" data-toggle="tab">雨量</a>
+						<a class="nav-link nav_a" href="#tab3" data-toggle="tab">雨量</a>
 					</li>
 					
 				</ul>
 				<div class="tab-content">
-					<div class="tab-pane active" id="tab1" >
+					<div class="tab-pane active tab" id="tab1" >
 						<p>
 						<?php require "two.php"?>
 						<div class="row">
@@ -172,19 +206,19 @@ if($row_rain["today"]!=$date){
 							<img alt="<?=$today?>" src="weather/<?=$today?>.jpg" class="rounded resize" >
 							<div>
 								<div class="card-block">
-									<h4>今日 <?php echo date("m/d");?></h4>
+									<h4 >今日 <?php echo date("m/d");?></h4>
 									<hr>
 									<h5 class="card-title">
-										<?php echo $row["wx"];?>
+										<?php if($row["wx"]==null){echo "沒有資訊";}else{echo $row["wx"];} ?>
 									</h5>
 									<p class="card-text">
-										<?php echo $row["pop"];?>
+										<?php if($row["pop"]==null){echo "沒有資訊";}else{echo $row["pop"];} ?>
 									</p>
 									<p class="card-text">
-										<?php echo $row["t"];?>
+										<?php if($row["t"]==null){echo "沒有資訊";}else{echo $row["t"];} ?>
 									</p>
 									<p class="card-text">
-										<?php echo $row["ws"];?>
+										<?php if($row["ws"]==null){echo "沒有資訊";}else{echo $row["ws"];} ?>
 									</p>
 								</div>
 							</div>
@@ -193,7 +227,7 @@ if($row_rain["today"]!=$date){
 							<img alt="<?=$torm?>" src="weather/<?=$torm?>.jpg" class="rounded resize" >
 								<div >
 									<div class="card-block">
-										<h4>明日 <?php echo date("m/d",strtotime("1 day"));?></h4>
+										<h4 >明日 <?php echo date("m/d",strtotime("1 day"));?></h4>
 										<hr>
 										<h5 class="card-title">
 											<?php echo $row_torm["wx"];?>
@@ -214,7 +248,7 @@ if($row_rain["today"]!=$date){
 						<img alt="<?=$acq?>" src="weather/<?=$acq?>.jpg" class="rounded resize" >
 							<div>
 								<div class="card-block">
-									<h4>後天 <?php echo date("m/d",strtotime("2 day"));?></h4>
+									<h4 class="tab_h4">後天 <?php echo date("m/d",strtotime("2 day"));?></h4>
 									<hr>
 									<h5 class="card-title">
 										<?php echo $row_acq["wx"];?>
@@ -239,132 +273,80 @@ if($row_rain["today"]!=$date){
 						<p>
 							<?php require "week.php"?>
 							<div class="row">
-							<div class="col-md-4">
-								<div class="card">
-									<div class="card-block">
-										<h5 class="card-title">
-											<?php echo $row_0["wx"];?>
-										</h5>
-										<p class="card-text">
-											<?php echo $row_0["pop"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_0["t"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_0["ci"];?>
-										</p>
-									</div>
-								</div>
+								<div class="container">
+								<table class="table table-striped table_dark">
+									<thead>
+									<tr>
+										<th> </th>
+										<th>日期</th>
+										<th>天氣</th>
+										<th>降雨機率</th>
+										<th>溫度區間</th>
+										<th>舒適度</th>
+									</tr>
+									</thead>
+									<tbody>
+									<tr>
+										<td><img alt="<?=$w0?>" src="weather/<?=$w0?>.jpg" class="rounded resize_week" ></td>
+										<td><?=date("m-d");?></td>
+										<td><?=$row_0["wx"]?></td>
+										<td><?=$row_0["pop"]?></td>
+										<td><?=$row_0["t"]?></td>
+										<td><?=$row_0["ci"]?></td>
+									</tr>
+									<tr>
+										<td><img alt="<?=$w1?>" src="weather/<?=$w1?>.jpg" class="rounded resize_week" ></td>
+										<td><?=date("m-d",strtotime("1 day"));?></td>
+										<td><?=$row_1["wx"]?></td>
+										<td><?=$row_1["pop"]?></td>
+										<td><?=$row_1["t"]?></td>
+										<td><?=$row_1["ci"]?></td>
+									</tr>
+									<tr>
+										<td><img alt="<?=$w2?>" src="weather/<?=$w2?>.jpg" class="rounded resize_week" ></td>
+										<td><?=date("m-d",strtotime("2 day"));?></td>
+										<td><?=$row_2["wx"]?></td>
+										<td><?=$row_2["pop"]?></td>
+										<td><?=$row_2["t"]?></td>
+										<td><?=$row_2["ci"]?></td>
+									</tr>
+									<tr>
+										<td><img alt="<?=$w3?>" src="weather/<?=$w3?>.jpg" class="rounded resize_week" ></td>
+										<td><?=date("m-d",strtotime("3 day"));?></td>
+										<td><?=$row_3["wx"]?></td>
+										<td><?php if($row_3["pop"]==null){echo "沒有資訊";}else{echo $row_3["pop"];} ?></td>
+										<td><?=$row_3["t"]?></td>
+										<td><?=$row_3["ci"]?></td>
+									</tr>
+									<tr>
+										<td><img alt="<?=$w4?>" src="weather/<?=$w4?>.jpg" class="rounded resize_week" ></td>
+										<td><?=date("m-d",strtotime("4 day"));?></td>
+										<td><?=$row_4["wx"]?></td>
+										<td><?php if($row_4["pop"]==null){echo "沒有資訊";}else{echo $row_4["pop"];} ?></td>
+										<td><?=$row_4["t"]?></td>
+										<td><?=$row_4["ci"]?></td>
+									</tr>
+									<tr>
+										<td><img alt="<?=$w5?>" src="weather/<?=$w5?>.jpg" class="rounded resize_week" ></td>
+										<td><?=date("m-d",strtotime("5 day"));?></td>
+										<td><?=$row_5["wx"]?></td>
+										<td><?php if($row_5["pop"]==null){echo "沒有資訊";}else{echo $row_5["pop"];} ?></td>
+										<td><?=$row_5["t"]?></td>
+										<td><?=$row_5["ci"]?></td>
+									</tr>
+									<tr>
+										<td><img alt="<?=$w6?>" src="weather/<?=$w6?>.jpg" class="rounded resize_week" ></td>
+										<td><?=date("m-d",strtotime("6 day"));?></td>
+										<td><?=$row_6["wx"]?></td>
+										<td><?php if($row_6["pop"]==null){echo "沒有資訊";}else{echo $row_6["pop"];} ?></td>
+										<td><?=$row_6["t"]?></td>
+										<td><?=$row_6["ci"]?></td>
+									</tr>
+
+									</tbody>
+								</table>
 							</div>
-							<div class="col-md-4">
-								<div class="card">
-									<div class="card-block">
-										<h5 class="card-title">
-											<?php echo $row_1["wx"];?>
-										</h5>
-										<p class="card-text">
-											<?php echo $row_1["pop"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_1["t"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_1["ci"];?>
-										</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-4">
-								<div class="card">
-									<div class="card-block">
-										<h5 class="card-title">
-											<?php echo $row_2["wx"];?>
-										</h5>
-										<p class="card-text">
-											<?php echo $row_2["pop"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_2["t"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_2["ci"];?>
-										</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-3">
-								<div class="card">
-									<div class="card-block">
-										<h5 class="card-title">
-											<?php echo $row_3["wx"];?>
-										</h5>
-										<p class="card-text">
-											<?php echo $row_3["pop"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_3["t"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_3["ci"];?>
-										</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-3">
-								<div class="card">
-									<div class="card-block">
-										<h5 class="card-title">
-											<?php echo $row_4["wx"];?>
-										</h5>
-										<p class="card-text">
-											<?php echo $row_4["pop"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_4["t"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_4["ci"];?>
-										</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-3">
-								<div class="card">
-									<div class="card-block">
-										<h5 class="card-title">
-											<?php echo $row_5["wx"];?>
-										</h5>
-										<p class="card-text">
-											<?php echo $row_5["pop"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_5["t"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_5["ci"];?>
-										</p>
-									</div>
-								</div>
-							</div>
-							<div class="col-md-3">
-								<div class="card">
-									<div class="card-block">
-										<h5 class="card-title">
-											<?php echo $row_6["wx"];?>
-										</h5>
-										<p class="card-text">
-											<?php echo $row_6["pop"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_6["t"];?>
-										</p>
-										<p class="card-text">
-											<?php echo $row_6["ci"];?>
-										</p>
-									</div>
-								</div>
-							</div>
+
 						</p>
 					</div>	
 				</div>
@@ -373,15 +355,15 @@ if($row_rain["today"]!=$date){
 						<p>
 						<div class="container">
 							<input class="form-control mb-4" id="tableSearch" type="text"
-								placeholder="Type something to search list items">
+								placeholder="輸入關鍵字查詢">
 
 							<table class="table table-bordered table-striped">
 								<thead>
 								<tr>
 									<th>localName</th>
 									<th>town</th>
-									<th>rain</th>
-									<th>hour_24</th>
+									<th>過去1小時(mm)</th>
+									<th>過去24小時(mm)</th>
 								</tr>
 								</thead>
 								<tbody id="myTable">
--- week.php
+++ week.php
@@ -38,6 +38,230 @@ for($x=0;$x<7;$x++){
 
     }
 }
+switch($row_0["wx"]){
+    case '晴':
+        $w0="sun";
+    break;
+    case '多雲':
+        $w0="clound_many";
+    break;
+    case ('晴時多雲'):
+        $w0="clound_sun";
+    break;
+    case ('多雲時晴'):
+        $w0="clound_sun";
+    break;
+    case '陰天':
+        $w0="clound";
+    break;
+    case '多雲時陰短暫陣雨':
+        $w0="rain";
+    break;
+    case '多雲短暫陣雨':
+        $w0="rain";
+    break;
+    case '陰短暫陣雨':
+        $w0="rain";
+    break;
+    case '情午後短暫雷陣雨':
+        $w0="thunder_rain";
+    break;
+    case '多雲午後短暫雷陣雨':
+        $w0="thunder_rain";
+    break;
+}
+switch($row_1["wx"]){
+    case '晴':
+        $w1="sun";
+    break;
+    case '多雲':
+        $w1="clound_many";
+    break;
+    case ('晴時多雲'):
+        $w1="clound_sun";
+    break;
+    case ('多雲時晴'):
+        $w1="clound_sun";
+    break;
+    case '陰天':
+        $w1="clound";
+    break;
+    case '多雲時陰短暫陣雨':
+        $w1="rain";
+    break;
+    case '多雲短暫陣雨':
+        $w1="rain";
+    break;
+    case '陰短暫陣雨':
+        $w1="rain";
+    break;
+    case '情午後短暫雷陣雨':
+        $w1="thunder_rain";
+    break;
+    case '多雲午後短暫雷陣雨':
+        $w1="thunder_rain";
+    break;
+}
+switch($row_2["wx"]){
+    case '晴':
+        $w2="sun";
+    break;
+    case '多雲':
+        $w2="clound_many";
+    break;
+    case ('晴時多雲'):
+        $w2="clound_sun";
+    break;
+    case ('多雲時晴'):
+        $w2="clound_sun";
+    break;
+    case '陰天':
+        $w2="clound";
+    break;
+    case '多雲時陰短暫陣雨':
+        $w2="rain";
+    break;
+    case '多雲短暫陣雨':
+        $w2="rain";
+    break;
+    case '陰短暫陣雨':
+        $w2="rain";
+    break;
+    case '情午後短暫雷陣雨':
+        $w2="thunder_rain";
+    break;
+    case '多雲午後短暫雷陣雨':
+        $w2="thunder_rain";
+    break;
+}
+switch($row_3["wx"]){
+    case '晴':
+        $w3="sun";
+    break;
+    case '多雲':
+        $w3="clound_many";
+    break;
+    case ('晴時多雲'):
+        $w3="clound_sun";
+    break;
+    case ('多雲時晴'):
+        $w3="clound_sun";
+    break;
+    case '陰天':
+        $w3="clound";
+    break;
+    case '多雲時陰短暫陣雨':
+        $w3="rain";
+    break;
+    case '多雲短暫陣雨':
+        $w3="rain";
+    break;
+    case '陰短暫陣雨':
+        $w3="rain";
+    break;
+    case '情午後短暫雷陣雨':
+        $w3="thunder_rain";
+    break;
+    case '多雲午後短暫雷陣雨':
+        $w3="thunder_rain";
+    break;
+}
+switch($row_4["wx"]){
+    case '晴':
+        $w4="sun";
+    break;
+    case '多雲':
+        $w4="clound_many";
+    break;
+    case ('晴時多雲'):
+        $w4="clound_sun";
+    break;
+    case ('多雲時晴'):
+        $w4="clound_sun";
+    break;
+    case '陰天':
+        $w4="clound";
+    break;
+    case '多雲時陰短暫陣雨':
+        $w4="rain";
+    break;
+    case '多雲短暫陣雨':
+        $w4="rain";
+    break;
+    case '陰短暫陣雨':
+        $w4="rain";
+    break;
+    case '情午後短暫雷陣雨':
+        $w4="thunder_rain";
+    break;
+    case '多雲午後短暫雷陣雨':
+        $w4="thunder_rain";
+    break;
+}
+switch($row_5["wx"]){
+    case '晴':
+        $w5="sun";
+    break;
+    case '多雲':
+        $w5="clound_many";
+    break;
+    case ('晴時多雲'):
+        $w5="clound_sun";
+    break;
+    case ('多雲時晴'):
+        $w5="clound_sun";
+    break;
+    case '陰天':
+        $w5="clound";
+    break;
+    case '多雲時陰短暫陣雨':
+        $w5="rain";
+    break;
+    case '多雲短暫陣雨':
+        $w5="rain";
+    break;
+    case '陰短暫陣雨':
+        $w5="rain";
+    break;
+    case '情午後短暫雷陣雨':
+        $w5="thunder_rain";
+    break;
+    case '多雲午後短暫雷陣雨':
+        $w5="thunder_rain";
+    break;
+}
+switch($row_6["wx"]){
+    case '晴':
+        $w6="sun";
+    break;
+    case '多雲':
+        $w6="clound_many";
+    break;
+    case ('晴時多雲'):
+        $w6="clound_sun";
+    break;
+    case ('多雲時晴'):
+        $w6="clound_sun";
+    break;
+    case '陰天':
+        $w6="clound";
+    break;
+    case '多雲時陰短暫陣雨':
+        $w6="rain";
+    break;
+    case '多雲短暫陣雨':
+        $w6="rain";
+    break;
+    case '陰短暫陣雨':
+        $w6="rain";
+    break;
+    case '情午後短暫雷陣雨':
+        $w6="thunder_rain";
+    break;
+    case '多雲午後短暫雷陣雨':
+        $w6="thunder_rain";
+    break;
+}
 
 
 
